98c98f75d60a79889dde5d2b4de87eb057bc29a2
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-04 18:42:04 +0200

    added use case for a literal func

--- func-literal/main.go
+++ func-literal/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	// Simple definition
@@ -12,5 +15,13 @@ func main() {
 	func(msg string) {
 		fmt.Println(msg)
 	}("Hello w/ args")
+
+	// Use case to sort a list
+	nums := []int{35, 55, 5, 15}
+	sort.Slice(nums, func(a,b int) bool {
+		return nums[a] < nums[b]
+	})
+	fmt.Println(nums) // [5 15 35 55]
+
 
 }
