e9cdd4afeafe0f9292db8e1b64ce1c17cd37a79e
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-04 18:15:48 +0200

    logged first messages

--- logging/main.go
+++ logging/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	// Create, Append, WriteOnly logs file
+	logsFile, err := os.OpenFile("logs.txt", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
+
+	if err != nil {
+		// Fatal makes program crash
+		log.Fatal(err)
+	}
+
+	log.SetOutput(logsFile)
+	log.Println("Hello there")
+	log.Println("Output here")
+}
